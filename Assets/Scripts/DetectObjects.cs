dedc48539922b903f6b2d201511e7f22cb745911
RachaelColaco <65530256+RachaelColaco@users.noreply.github.com>
2021-07-30 11:24:16 +1200

    detecting the layer of object

--- Assets/Scripts/DetectObjects.cs
+++ Assets/Scripts/DetectObjects.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class DetectObjects : MonoBehaviour
 {
+
+    [SerializeField] private LayerMask objects;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,17 +16,18 @@ public class DetectObjects : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Clicking down on a object 
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit))
+            //checking if its a evidence object
+            if (Physics.Raycast(ray, out RaycastHit hit, objects))
             {
 
                 //Select stage    
-                if (hit.collider.name == "Cube")
+                if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Evidence"))
                 {
-                    Debug.Log("its a cube");
+                    Debug.Log("its a evudence");
                 }
             }
         }
