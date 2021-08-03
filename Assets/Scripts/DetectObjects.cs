2196e369183e1a1380eb2d9e9a187164d63ee369
RachaelColaco <65530256+RachaelColaco@users.noreply.github.com>
2021-08-03 13:00:57 +1200

    mainscene

--- Assets/Scripts/DetectObjects.cs
+++ Assets/Scripts/DetectObjects.cs
@@ -7,7 +7,7 @@ public class DetectObjects : MonoBehaviour
 
     [SerializeField] private LayerMask objects;
     private Camera _camera;
-
+    [SerializeField] TextAsset story;
 
     // Start is called before the first frame update
     void Start()
@@ -29,6 +29,10 @@ public class DetectObjects : MonoBehaviour
                 if (hit.collider.TryGetComponent<EvidenceInfo>(out EvidenceInfo evidence))
                 {
                     gameManager.Instance.addEvidence(evidence);
+                }
+                else if(hit.collider.gameObject.layer == LayerMask.NameToLayer("Suspect"))
+                {
+                    gameManager.Instance.StartStory(story);
                 }
             }
         }
